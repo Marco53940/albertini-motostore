1d71ddfd7f33cc99be1c8caee8bce6def45bb7d7
Marco Roberto Albertini <malbertini@trimix.com.ar>
2022-11-06 19:59:11 -0300

    Sprint 4 TP 1

---
+++ create_database.sql
@@ -0,0 +1,55 @@
+CREATE SCHEMA `motostore` ;
+
+CREATE TABLE `motostore`.`users` (
+  `id_user` INT NOT NULL,
+  `name` VARCHAR(45) NULL,
+  `lastname` VARCHAR(45) NULL,
+  `email` VARCHAR(45) NULL,
+  `username` VARCHAR(100) NULL,
+  `password` VARCHAR(100) NULL,
+  `birthday` VARCHAR(45) NULL,
+  PRIMARY KEY (`id_user`));
+
+ 
+ INSERT INTO `motostore`.`users`
+(`id_user`,
+`name`,
+`lastname`,
+`email`,
+`username`,
+`password`,
+`birthday`)
+VALUES
+(1,
+'Marco',
+'Albertini',
+'marlbertini@gmail.com',
+'Marco',
+'$2b$10$66zB63k.oDOYChKChqXzDu1/hBchq0Z8.BBlalchu4bz4RTQSFP9G',
+'30-10-1989');
+
+CREATE TABLE `motostore`.`products` (
+  `id_product` INT NOT NULL,
+  `name` VARCHAR(45) NULL,
+  `description` VARCHAR(45) NULL,
+  `price` INT NULL,
+  PRIMARY KEY (`id_product`));
+
+ INSERT INTO `motostore`.`products`
+(`id_product`,
+`name`,
+`description`,
+`price`)
+VALUES
+(1,
+'Casco',
+'Agv Corsa',
+300),
+(2,
+'Guantes',
+'Dainese Ultra Corsa',
+10),
+(3,
+'Botas',
+'Dainses Torque In',
+200);
